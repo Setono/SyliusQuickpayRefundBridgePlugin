22b840ff665bbbb9f77b957f025e9c2f56ebf8f1
Joachim Løvgaard <joachim@loevgaard.dk>
2022-04-25 12:25:43 +0200

    Log errors in handler

--- src/Command/Handler/RefundUnitsHandler.php
+++ src/Command/Handler/RefundUnitsHandler.php
@@ -6,15 +6,21 @@ namespace Setono\SyliusQuickpayRefundBridgePlugin\Command\Handler;
 
 use Payum\Core\Registry\RegistryInterface;
 use Payum\Core\Request\Refund;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 use Setono\SyliusQuickpayRefundBridgePlugin\Command\RefundUnits;
 use Setono\SyliusQuickpayRefundBridgePlugin\Command\Validator\RefundUnitsCommandValidatorInterface;
 use Sylius\Component\Core\Model\PaymentInterface;
 use Sylius\Component\Core\Model\PaymentMethodInterface;
 use Sylius\Component\Core\Repository\PaymentRepositoryInterface;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 use Webmozart\Assert\Assert;
 
-final class RefundUnitsHandler
+final class RefundUnitsHandler implements MessageHandlerInterface, LoggerAwareInterface
 {
+    private LoggerInterface $logger;
+
     private RegistryInterface $payum;
 
     private RefundUnitsCommandValidatorInterface $commandValidator;
@@ -26,6 +32,7 @@ final class RefundUnitsHandler
         RefundUnitsCommandValidatorInterface $commandValidator,
         PaymentRepositoryInterface $paymentRepository
     ) {
+        $this->logger = new NullLogger();
         $this->payum = $payum;
         $this->commandValidator = $commandValidator;
         $this->paymentRepository = $paymentRepository;
@@ -33,41 +40,62 @@ final class RefundUnitsHandler
 
     public function __invoke(RefundUnits $command): void
     {
-        $this->commandValidator->validate($command);
+        try {
+            $this->commandValidator->validate($command);
 
-        if (null === ($paymentId = $command->paymentId())) {
-            return;
+            if (null === ($paymentId = $command->paymentId())) {
+                return;
+            }
+
+            $payment = $this->paymentRepository->find($paymentId);
+
+            if (!($payment instanceof PaymentInterface)) {
+                return;
+            }
+
+            if (!isset($payment->getDetails()['quickpayPaymentId'])) {
+                return;
+            }
+
+            if (null === $paymentMethod = $payment->getMethod()) {
+                return;
+            }
+
+            Assert::isInstanceOf($paymentMethod, PaymentMethodInterface::class);
+
+            $gatewayConfig = $paymentMethod->getGatewayConfig();
+
+            if (null === $gatewayConfig || $gatewayConfig->getFactoryName() !== 'quickpay') {
+                return;
+            }
+
+            $params = [
+                'quickpayPaymentId' => $payment->getDetails()['quickpayPaymentId'],
+                'amount' => $command->amount(),
+            ];
+
+            $gatewayFactory = $this->payum->getGatewayFactory('quickpay');
+            $gateway = $gatewayFactory->create($gatewayConfig->getConfig());
+            $gateway->execute(new Refund($params));
+        } catch (\Throwable $e) {
+            $this->logger->error(self::exceptionMessage($e));
+
+            throw $e;
         }
+    }
 
-        $payment = $this->paymentRepository->find($paymentId);
+    public function setLogger(LoggerInterface $logger): void
+    {
+        $this->logger = $logger;
+    }
 
-        if (!($payment instanceof PaymentInterface)) {
-            return;
-        }
-
-        if (!isset($payment->getDetails()['quickpayPaymentId'])) {
-            return;
-        }
-
-        if (null === $paymentMethod = $payment->getMethod()) {
-            return;
-        }
-
-        Assert::isInstanceOf($paymentMethod, PaymentMethodInterface::class);
-
-        $gatewayConfig = $paymentMethod->getGatewayConfig();
-
-        if (null === $gatewayConfig || $gatewayConfig->getFactoryName() !== 'quickpay') {
-            return;
-        }
-
-        $params = [
-            'quickpayPaymentId' => $payment->getDetails()['quickpayPaymentId'],
-            'amount' => $command->amount(),
-        ];
-
-        $gatewayFactory = $this->payum->getGatewayFactory('quickpay');
-        $gateway = $gatewayFactory->create($gatewayConfig->getConfig());
-        $gateway->execute(new Refund($params));
+    private static function exceptionMessage(\Throwable $e): string
+    {
+        return sprintf(
+            "[%s] %s\n\nTrace\n---\n%s\n\n",
+            get_class($e),
+            $e->getMessage(),
+            $e->getTraceAsString()
+        );
     }
 }
